f387f7bcc27dda0de6f1abf031218a91fa2ee5dd
Julia Yerman <juliyaerm88@gmail.com>
2023-07-17 18:34:28 -0700

    Setup & Routing Basics/Loaders

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,18 @@
+// rrd imports
+import { useLoaderData } from "react-router-dom";
+
+// helper functions
+import { fetchData } from "./helpers";
+
+// loader
+export function dashboardLoader() {
+  const userName = fetchData("userName");
+  return { userName };
+}
+
+const Dashboard = () => {
+  const { userName } = useLoaderData();
+  return <div>Dashboard</div>;
+};
+
+export default Dashboard;
