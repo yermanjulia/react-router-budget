38a21229eaef829fb60327e624009d9dc1f3d5f6
Julia Yerman <juliyaerm88@gmail.com>
2023-07-28 22:19:57 -0700

    Added a Login Form

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,12 @@
 // rrd imports
 import { useLoaderData } from "react-router-dom";
 
+// library imports
+import { toast } from "react-toastify";
+
+// components
+import Intro from "../components/Intro";
+
 //  helper functions
 import { fetchData } from "../helpers";
 
@@ -9,15 +15,20 @@ export function dashboardLoader() {
   const userName = fetchData("userName");
   return { userName };
 }
+// action
+export async function dashboardAction({ request }) {
+  const data = await request.formData();
+  const formData = Object.fromEntries(data);
+  try {
+    throw new Error("Ya done");
+  } catch (e) {
+    throw new Error("There was a problem creating your account.");
+  }
+}
 
 const Dashboard = () => {
   const { userName } = useLoaderData();
 
-  return (
-    <div>
-      <h1>{userName}</h1>
-      Dashboard
-    </div>
-  );
+  return <>{userName ? <p>{userName}</p> : <Intro />}</>;
 };
 export default Dashboard;
