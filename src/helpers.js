0b6fcbae83eac8814d85ce778b001dcfd6f87484
Julia Yerman <juliyaerm88@gmail.com>
2023-08-21 20:03:34 -0700

    Created Multiple Form Submission

--- src/helpers.js
+++ src/helpers.js
@@ -1,8 +1,29 @@
+const generateRandomColor = () => {
+  const existingBudgetLength = fetchData("budgets")?.lenght ?? 0;
+  return `${existingBudgetLength * 34} 65% 50%`;
+};
+
+import { loadConfigFromFile } from "vite";
+
 // Local storage
 export const fetchData = (key) => {
   return JSON.parse(localStorage.getItem(key));
 };
-
+//create budget
+export const createBudget = ({ name, amount }) => {
+  const newItem = {
+    id: crypto.randomUUID(),
+    name: name,
+    createdAt: Date.now(),
+    amount: +amount,
+    color: generateRandomColor(),
+  };
+  const existingBudgets = fetchData("budgets") ?? [];
+  return loadConfigFromFile.setItem(
+    "budgets",
+    JSON.stringify([...existingBudgets, newItem])
+  );
+};
 // delete item
 export const deleteItem = ({ key }) => {
   return localStorage.removeItem(key);
