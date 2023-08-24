93c53f32af9db2f6c7ef171824eb4f04179c7128
Julia Yerman <juliyaerm88@gmail.com>
2023-08-24 12:38:10 -0700

    Added amount form

--- src/components/AddExpenseForm.jsx
+++ src/components/AddExpenseForm.jsx
@@ -1,4 +1,14 @@
+//react imports
+import { useRef } from "react";
+
+//react router dom imports
+import { useFetcher } from "react-router-dom";
+
 const AddExpenseForm = ({ budgets }) => {
+  const fetcher = useFetcher();
+
+  const formRef = useRef();
+  const focusRef = useRef();
   return (
     <div className="form-wrapper">
       <h2 className="h3">
@@ -6,9 +16,51 @@ const AddExpenseForm = ({ budgets }) => {
         <span className="accent">
           {" "}
           {budgets.length === 1 && `${budgets.map((budg) => budg.name)}`}
-        </span>
+        </span>{" "}
         Expense
       </h2>
+      <fetcher.Form method="post" className="grid-sm" ref={formRef}>
+        <div className="expense-inputs">
+          <div className="grid-xs">
+            <label htmlFor="newExpense"> Expense Name </label>
+
+            <input
+              type="text"
+              name="newExpense"
+              id="newExpense"
+              placeholder="e.g., Coffee"
+              ref={focusRef}
+              required
+            />
+          </div>
+          <div className="grid-xs">
+            <label htmlFor="newExpenseAmount">Amount</label>
+            <input
+              type="number"
+              step="0.01"
+              inputMode="decimal"
+              name="newExpenseAmount"
+              id="newExpenseAmount"
+              placeholder="e.g.,3.50"
+              required
+            />
+          </div>
+        </div>
+        <div className="grid-sm" hidden={budgets.length === 1}>
+          <label htmlFor="newExpenseBudget">Budget Category</label>
+          <select name="newExpenseBudget" id="newExpenseBudget" required>
+            {budgets
+              .sort((a, b) => a.createdAt - b.createdAt)
+              .map((budget) => {
+                return (
+                  <option key={budget.id} value={budget.id}>
+                    {budget.name}
+                  </option>
+                );
+              })}
+          </select>
+        </div>
+      </fetcher.Form>
     </div>
   );
 };
